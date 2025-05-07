181775b20321cc802a8801bf2889cffad99d8f1e
sumu9897 <sumon.bncd@gmail.com>
2025-05-07 21:53:17 +0600

    Problem 2 Solve

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,22 @@
     return toUpper ? input.toUpperCase() : input.toLowerCase();
   }
 
-  console.log(formatString("Hello"));
-  console.log(formatString("Hello", true));
-  console.log(formatString("Hello", false));
+  //   console.log(formatString("Hello"));
+  //   console.log(formatString("Hello", true));
+  //   console.log(formatString("Hello", false));
 
+  function filterByRating(
+    items: { title: string; rating: number }[]
+  ): { title: string; rating: number }[] {
+    return items.filter((item) => item.rating >= 4);
+  }
 
-
+  const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+  ];
+  console.log("Filtered Books:", filterByRating(books));
 
 
 }
